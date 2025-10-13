71df53fa9cdf36853cfe0ab71d76960e459792aa
Annsrutto <annsrutto567@gmail.com>
2025-10-14 00:54:55 +0300

    My first Cypress test case

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,2 +1,9 @@
 // Cypress - Spec
+describe('My First Test Suite', function() {
+    it('My firstTest case', function() {
+// Test Step
+cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+cy.title().should('include', 'GreenKart');
 
+    })
+})
