00dd22216e887a3321cd53dbb69c2d6a1e080f13
Annsrutto <annsrutto567@gmail.com>
2025-10-15 21:50:11 +0300

    promise handling and command chaining

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,9 +1,34 @@
-// Cypress - Spec
+/// <reference types="Cypress" />
+
 describe('My First Test Suite', function() {
     it('My firstTest case', function() {
-// Test Step
-cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-cy.title().should('include', 'GreenKart');
+    // Test Step
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.title().should('include', 'GreenKart');
 
+    cy.get('.search-keyword').type('ca');
+    cy.wait(2000);
+
+    // Handling invivible items
+    cy.get('.product:visible').should('have.length', 4);
+
+    // Parent-Child Chaining
+    cy.get('.products').find('.product').should('have.length', 4);
+
+    // How to select an item and add to cart
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
+
+    // How to dynamically add items to cart
+    cy.get('.products').find('.product').each(($el, index, $list) => {
+
+        const vegtext=$el.find('h4.product-name').text()
+        if (vegtext.includes('Cashews')) {
+            cy.wrap($el).find('button').click()
+        }
+    })
+    // Manually resolving storing a variable in Cypress
+    cy.get('.brand').then((logoelement) =>{
+        cy.log(logoelement.text())
+    })
     })
 })
